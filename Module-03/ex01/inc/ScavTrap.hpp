08447914b2fd7abdec04aec9e3dcf09d3989815d
Federico Lo Dolce <flodolce92@gmail.com>
2025-03-06 02:33:38 +0100

    added module 03 ex01

---
+++ Module-03/ex01/inc/ScavTrap.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 00:48:58 by flo-dolc          #+#    #+#             */
+/*   Updated: 2025/03/06 02:26:02 by flo-dolc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap: public ClapTrap
+{
+	private:
+		bool	isGuardingGate;
+
+	public:
+		// Constructors and destructor
+		ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap &src);
+		~ScavTrap();
+
+		// Operator overloads
+		ScavTrap	&operator=(const ScavTrap &src);
+
+		// Methods
+		std::string	getType() const;
+		void		attack(std::string const &target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+		void		guardGate();
+};
+
+#endif
