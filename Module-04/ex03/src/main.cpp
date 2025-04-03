2b292831d52fb548b3559b53aefaa2ad1c6ef725
Federico Lo Dolce <flodolce92@gmail.com>
2025-04-03 21:35:46 +0200

    module 04 ex03 part3

--- Module-04/ex03/src/main.cpp
+++ Module-04/ex03/src/main.cpp
@@ -6,93 +6,31 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 18:14:58 by flo-dolc          #+#    #+#             */
-/*   Updated: 2025/04/03 20:58:03 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2025/04/03 21:26:22 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Ice.hpp"
 #include "Cure.hpp"
 #include "Character.hpp"
+#include "MateriaSource.hpp"
 
 int main()
 {
-	Character *me = new Character("me");
-	Character *bob = new Character("bob");
+	IMateriaSource *src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	ICharacter *me = new Character("me");
 	AMateria *tmp;
-	tmp = new Ice();
+	tmp = src->createMateria("ice");
 	me->equip(tmp);
-	tmp = new Cure();
+	tmp = src->createMateria("cure");
 	me->equip(tmp);
-	tmp = new Ice();
-	me->equip(tmp);
-	tmp = new Cure();
-	me->equip(tmp);
-
-	tmp = new Ice();
-	if (me->isInventoryFull())
-		delete tmp;
-	else
-		me->equip(tmp);
-
+	ICharacter *bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
-	me->use(2, *bob);
-	me->use(3, *bob);
-
-	// Unequip, delete and use
-	tmp = me->getInventoryItem(0);
-	me->unequip(0);
-	delete tmp;
-
-	tmp = me->getInventoryItem(1);
-	me->unequip(1);
-	delete tmp;
-
-	// Bob inventory
-	tmp = bob->getInventoryItem(0);
-	if (tmp)
-		std::cout << "Bob inventory 0: " << tmp->getType() << std::endl;
-	else
-		std::cout << "Bob inventory 0: NULL" << std::endl;
-	tmp = bob->getInventoryItem(1);
-	if (tmp)
-		std::cout << "Bob inventory 1: " << tmp->getType() << std::endl;
-	else
-		std::cout << "Bob inventory 1: NULL" << std::endl;
-	tmp = bob->getInventoryItem(2);
-	if (tmp)
-		std::cout << "Bob inventory 2: " << tmp->getType() << std::endl;
-	else
-		std::cout << "Bob inventory 2: NULL" << std::endl;
-	tmp = bob->getInventoryItem(3);
-	if (tmp)
-		std::cout << "Bob inventory 3: " << tmp->getType() << std::endl;
-	else
-		std::cout << "Bob inventory 3: NULL" << std::endl;
-
-	// Me inventory
-	tmp = me->getInventoryItem(0);
-	if (tmp)
-		std::cout << "Me inventory 0: " << tmp->getType() << std::endl;
-	else
-		std::cout << "Me inventory 0: NULL" << std::endl;
-	tmp = me->getInventoryItem(1);
-	if (tmp)
-		std::cout << "Me inventory 1: " << tmp->getType() << std::endl;
-	else
-		std::cout << "Me inventory 1: NULL" << std::endl;
-	tmp = me->getInventoryItem(2);
-	if (tmp)
-		std::cout << "Me inventory 2: " << tmp->getType() << std::endl;
-	else
-		std::cout << "Me inventory 2: NULL" << std::endl;
-	tmp = me->getInventoryItem(3);
-	if (tmp)
-		std::cout << "Me inventory 3: " << tmp->getType() << std::endl;
-	else
-		std::cout << "Me inventory 3: NULL" << std::endl;
-
 	delete bob;
 	delete me;
-	return (0);
+	delete src;
+	return 0;
 }
