9e441ed752cbba8140e4afc9e6cbc4ad66d95648
Federico Lo Dolce <flodolce92@gmail.com>
2025-04-11 16:21:13 +0200

    fix: main and general fixes

--- Module-03/ex01/src/main.cpp
+++ Module-03/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 01:31:25 by flo-dolc          #+#    #+#             */
-/*   Updated: 2025/03/06 18:28:24 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2025/03/09 17:30:10 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,29 @@ int	main()
 	scav1.beRepaired(20);
 	scav2.beRepaired(30);
 
+	std::cout << std::endl;
 	scav1.guardGate();
+	std::cout << std::endl;
 	scav2.guardGate();
+	std::cout << std::endl;
 	scav3.guardGate();
+	std::cout << std::endl;
 	scav1.guardGate();
+	std::cout << std::endl;
 
 	scav1 = scav2;
 	scav1.attack("P1N0");
+
+	std::cout << std::endl;
+	std::cout << scav1.getType() << " " << scav1.getName()
+			  << " has " << scav1.getHitPoints() << " hit points and "
+			  << scav1.getEnergyPoints() << " energy points" << std::endl;
+	std::cout << scav2.getType() << " " << scav2.getName()
+			  << " has " << scav2.getHitPoints() << " hit points and "
+			  << scav2.getEnergyPoints() << " energy points" << std::endl;
+	std::cout << scav3.getType() << " " << scav3.getName()
+			  << " has " << scav3.getHitPoints() << " hit points and "
+			  << scav3.getEnergyPoints() << " energy points" << std::endl;
 
 	return (0);
 }
--- Module-03/ex02/src/main.cpp
+++ Module-03/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 17:22:56 by flo-dolc          #+#    #+#             */
-/*   Updated: 2025/03/06 18:31:33 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2025/03/09 17:31:09 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,27 @@ int main()
 	frag1.beRepaired(20);
 	frag2.beRepaired(30);
 
+	std::cout << std::endl;
 	frag1.highFivesGuys();
+	std::cout << std::endl;
 	frag2.highFivesGuys();
+	std::cout << std::endl;
 	frag3.highFivesGuys();
+	std::cout << std::endl;
 
 	frag1 = frag2;
 	frag1.attack("P1N0");
+
+	std::cout << std::endl;
+	std::cout << frag1.getType() << " " << frag1.getName()
+			  << " has " << frag1.getHitPoints() << " hit points and "
+			  << frag1.getEnergyPoints() << " energy points" << std::endl;
+	std::cout << frag2.getType() << " " << frag2.getName()
+			  << " has " << frag2.getHitPoints() << " hit points and "
+			  << frag2.getEnergyPoints() << " energy points" << std::endl;
+	std::cout << frag3.getType() << " " << frag3.getName()
+			  << " has " << frag3.getHitPoints() << " hit points and "
+			  << frag3.getEnergyPoints() << " energy points" << std::endl;
 
 	return (0);
 }
