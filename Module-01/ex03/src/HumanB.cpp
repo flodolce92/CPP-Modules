472c25b9bf1cf12d6f5c7442126ec444237e8a55
Federico Lo Dolce <flodolce92@gmail.com>
2025-04-11 15:27:45 +0200

    fix: Humans

--- Module-01/ex03/src/HumanB.cpp
+++ Module-01/ex03/src/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 00:55:34 by flo-dolc          #+#    #+#             */
-/*   Updated: 2025/01/27 00:58:44 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2025/03/04 23:01:41 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,11 @@ HumanB::~HumanB()
 
 void	HumanB::attack()
 {
-	std::cout << _name << " attacks with their "
-				<< _weapon->getType() << std::endl;
+	if (_weapon == NULL)
+		std::cout << _name << " has no weapon to attack with" << std::endl;
+	else
+		std::cout << _name << " attacks with their "
+					<< _weapon->getType() << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon *weapon)
