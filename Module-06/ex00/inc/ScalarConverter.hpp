e20b8fcc0f0292acb9e48d231986e4af4fada6a3
Federico Lo Dolce <flodolce92@gmail.com>
2025-04-14 17:54:16 +0200

    added check for chars

--- Module-06/ex00/inc/ScalarConverter.hpp
+++ Module-06/ex00/inc/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:17:34 by flo-dolc          #+#    #+#             */
-/*   Updated: 2025/04/14 16:33:05 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2025/04/17 03:37:26 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define SCALARCONVERTER_HPP
 
 # include "debug.hpp"
+# include <cctype>
+# include <iomanip>
 
 class ScalarConverter
 {
@@ -32,5 +34,12 @@ class ScalarConverter
 		// Methods
 		static void convert(std::string literal);
 };
+
+// Helper Functions
+int charCase(std::string &literal);
+void intCase(std::string &literal);
+void floatCase(std::string &literal);
+void doubleCase(std::string &literal);
+int quoteForm(std::string &literal);
 
 #endif
--- Module-06/ex00/src/ScalarConverter.cpp
+++ Module-06/ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:33:35 by flo-dolc          #+#    #+#             */
-/*   Updated: 2025/04/14 17:48:37 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2025/04/17 03:46:18 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 // Constructors and destructor
 ScalarConverter::ScalarConverter(const ScalarConverter &src)
 {
-	(void) src;
+	(void)src;
 }
 
 ScalarConverter::~ScalarConverter() {}
@@ -23,12 +23,45 @@ ScalarConverter::~ScalarConverter() {}
 // Operator overloads
 ScalarConverter &ScalarConverter::operator=(const ScalarConverter &src)
 {
-	(void) src;
+	(void)src;
 	return (*this);
 }
 
 // Methods
 void ScalarConverter::convert(std::string literal)
 {
-	std::cout << "Literal: " << literal << std::endl;
+	std::cout << "Literal: " << literal << " len: " << literal.length() << std::endl;
+	if (literal.length() == 1 || quoteForm(literal) == 1)
+		if (charCase(literal) == 1)
+			return;
+
+	std::cout << "char: impossible\n"
+			  << "int: impossible\n"
+			  << "float: impossible\n"
+			  << "double: impossible\n";
+}
+
+// Helper Functions
+int charCase(std::string &literal)
+{
+	char c = literal[0];
+
+	if (quoteForm(literal) == 1)
+		c = literal[1];
+
+	if (std::isprint(c) && !std::isdigit(c))
+	{
+		std::cout << "char: \'" << c << "\'\n"
+				  << "int: " << static_cast<int>(c) << "\n"
+				  << std::fixed << std::setprecision(1)
+				  << "float: " << static_cast<float>(c) << "f\n"
+				  << "double: " << static_cast<double>(c) << "\n";
+		return (1);
+	}
+	return (0);
+}
+
+int quoteForm(std::string &literal)
+{
+	return (literal.length() == 3 && literal[0] == '\'' && literal[2] == '\'');
 }
