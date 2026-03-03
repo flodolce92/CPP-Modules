85ffd7a23777c298d655bbe7d56f1b36f7a15185
Federico Lo Dolce <flodolce92@gmail.com>
2026-03-03 16:34:17 +0100

    fix: bsp fix

--- Module-02/ex03/src/bsp.cpp
+++ Module-02/ex03/src/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 17:06:06 by flo-dolc          #+#    #+#             */
-/*   Updated: 2025/05/06 20:02:54 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2026/03/03 16:19:11 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ bool	bsp( Point const a, Point const b, Point const c, Point const point)
 	Fixed	w2((point.getY() - a.getY() - w1.toFloat() * ba_y.toFloat()) /
 				ca_y.toFloat());
 
-	if (w1 > 0 && w1 > 0 && (w1 + w2) < 1)
+	if (w1 > 0 && w2 > 0 && (w1 + w2) < 1)
 		return (true);
 	return (false);
 }
